2f63edf28b084a0a2abe1ea3fb8eb0b9940fa77c
Steven Persia <persia.steven8@gmail.com>
2018-11-26 13:00:00 +0100

    Add List and Create feature

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		tasks: []
+	};
+
+	/* Render all tasks */
+	renderTasks = () => {
+		const { tasks } = this.state;
+		const results = tasks.map(task => (
+			<li key={task._id} className="task">
+				<span className="delete">
+					<i className="fas fa-times" />
+				</span>
+				<span className="title-task" onClick={this.handleUpdate} id={task._id}>
+					{task.title}
+				</span>
+			</li>
+		));
+		return results;
+	};
+
+	/* Save input */
+	handleInputChange = e => {
+		const target = e.target;
+		const value = target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	/* Add new task */
+	onSubmit = e => {
+		axios
+			.post('http://localhost:3000/create', {
+				title: this.state.title,
+				done: false
+			})
+			.then(response => {
+				const newStateTasks = [...this.state.tasks];
+				this.setState({
+					tasks: newStateTasks
+				});
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		e.preventDefault();
+	};
+
+	/* Update a task (done/undone) */
+	handleUpdate = e => {
+		axios
+			.post('http://localhost:3000/update', {
+				done: true
+			})
+			.then(response => {})
+			.catch(err => {
+				console.log(err);
+			});
+		e.preventDefault();
+	};
+
+	render() {
+		return (
+			<div className="tasks-list">
+				<h1>TODO LIST</h1>
+				<ul>{this.renderTasks()}</ul>
+				<form onSubmit={this.onSubmit}>
+					<input
+						name="title"
+						onChange={this.handleInputChange}
+						placeholder="Nom de la tâche"
+					/>
+					<button>Ajouter un tâche</button>
+				</form>
+			</div>
+		);
+	}
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:3000/')
+			.then(response => {
+				this.setState({
+					tasks: response.data
+				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 }
 
 export default App;
